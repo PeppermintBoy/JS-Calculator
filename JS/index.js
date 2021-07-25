cf97593d551ad08231ae4586b79d43d7dab12e84
yoyo <yoyointheho@gmail.com>
2021-07-25 15:54:27 +0900

    updated to have basic calculation function.

--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,50 @@ function divide(a, b) {
 function operate(firstNumber, secondNumber, operator){
     return operator(firstNumber, secondNumber);
 }
+
+//Inputted number shows on screen
+//store that inputted number
+//Add the first and second number when an operator is pressed.
+
+const screen = document.querySelector('.digits');
+const keys = document.querySelectorAll('.key');
+let array = [];
+let array2 = [];
+let operator;
+
+keys.forEach(key => key.addEventListener('click', operate));
+
+
+function operate(e) {
+    if (e.target.textContent == '+' || e.target.textContent == '-' || e.target.textContent == 'x' || e.target.textContent == '÷') {
+        operator = e.target.textContent;
+        array2 = array;
+        array = [];
+        console.log(array);
+        console.log(array2);
+        return screen.value = '0';
+    }
+    else if (e.target.textContent == '=') {
+        const joinedArray = parseInt(array.join(''));
+        const joinedArray2 = parseInt(array2.join(''))
+        if (operator == '+') {
+            return screen.value = add(joinedArray2, joinedArray);
+        } 
+        else if (operator == '-') {
+            return screen.value = subtract(joinedArray2, joinedArray);
+        }  
+        else if (operator == 'x') {
+            return screen.value = multiply(joinedArray2, joinedArray);
+        }
+        else if (operator == '÷') {
+            return screen.value = divide(joinedArray2, joinedArray);
+        }
+         
+    }
+    console.log(e.target.textContent);
+    let number = e.target.textContent;
+    array.push(number);
+    let joinedArray = array.join('');
+    return screen.value = joinedArray;
+} 
+
